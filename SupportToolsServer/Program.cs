34ec264c641389d023cacc8b7274079a1ed0e55e
merabza <merabza@gmail.com>
2025-02-04 22:45:32 +0400

    202502042245

--- SupportToolsServer/Program.cs
+++ SupportToolsServer/Program.cs
@@ -1,22 +1,31 @@
 //Created by ApiProgramClassCreator at 2/4/2025 7:31:10 PM
 
-using ConfigurationEncrypt;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Serilog;
-using SignalRMessages.Installers;
-using SwaggerTools;
 using System;
 using System.Collections.Generic;
-using WebInstallers;
+using System.Reflection;
+using ConfigurationEncrypt;
+using Figgle;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using SwaggerTools;
+using WebInstallers;
+using AssemblyReference = SupportToolsServerDb.AssemblyReference;
 
 try
 {
+    Console.WriteLine("Loading...");
+
+    const string appName = "Support Tools Server";
+
+    var header = $"{appName} {Assembly.GetEntryAssembly()?.GetName().Version}";
+    Console.WriteLine(FiggleFonts.Standard.Render(header));
+
+
     var parameters = new Dictionary<string, string>
     {
         { ConfigurationEncryptInstaller.AppKeyKey, "3081adaf7a5d472a88cd5149671a1922" },
-        { SwaggerInstaller.AppNameKey, "Support Tools Server" },
+        { SwaggerInstaller.AppNameKey, appName },
         { SwaggerInstaller.VersionCountKey, 1.ToString() },
         { SwaggerInstaller.UseSwaggerWithJwtBearerKey, string.Empty } //Allow Swagger
     };
@@ -32,26 +41,21 @@ try
     if (!builder.InstallServices(debugMode, args, parameters,
 
 //SupportToolsServerDbPart
-            SupportToolsServerDb.AssemblyReference.Assembly, SupportToolsServerRepositories.AssemblyReference.Assembly,
+            AssemblyReference.Assembly,
 
 //WebSystemTools
             ApiExceptionHandler.AssemblyReference.Assembly, ConfigurationEncrypt.AssemblyReference.Assembly,
             SerilogLogger.AssemblyReference.Assembly, StaticFilesTools.AssemblyReference.Assembly,
             SwaggerTools.AssemblyReference.Assembly, TestToolsApi.AssemblyReference.Assembly,
             WindowsServiceTools.AssemblyReference.Assembly))
-    {
         return 2;
-    }
 
 
     //ReSharper disable once using
 
     using var app = builder.Build();
 
-    if (!app.UseServices(debugMode))
-    {
-        return 1;
-    }
+    if (!app.UseServices(debugMode)) return 1;
 
     app.Run();
     return 0;
---
+++ SupportToolsServerApi/Endpoints/V1/ProjectsEndpoints.cs
@@ -0,0 +1,184 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SupportToolsServerContracts.V1.Routes;
+using WebInstallers;
+
+namespace SupportToolsServerApi.Endpoints.V1;
+
+
+public sealed class ProjectsEndpoints : IInstaller
+{
+    public int InstallPriority => 50;
+    public int ServiceUsePriority => 50;
+
+    public bool InstallServices(WebApplicationBuilder builder, bool debugMode, string[] args,
+        Dictionary<string, string> parameters)
+    {
+        return true;
+    }
+
+    public bool UseServices(WebApplication app, bool debugMode)
+    {
+        if (debugMode)
+            Console.WriteLine($"{GetType().Name}.{nameof(UseServices)} Started");
+
+        var group = app.MapGroup(SupportToolsServerApiRoutes.ApiBase + SupportToolsServerApiRoutes.Projects.ProjectBase)
+            .RequireAuthorization();
+
+        //group.MapGet(ProjectsApiRoutes.Projects.GetAppSettingsVersion, GetAppSettingsVersion);
+        //group.MapGet(ProjectsApiRoutes.Projects.GetVersion, GetVersion);
+        //group.MapDelete(ProjectsApiRoutes.Projects.RemoveProjectService, RemoveProjectService);
+        //group.MapPost(ProjectsApiRoutes.Projects.StartService, StartService);
+        //group.MapPost(ProjectsApiRoutes.Projects.StopService, StopService);
+        //group.MapPost(ProjectsApiRoutes.Projects.Update, Update);
+        //group.MapPost(ProjectsApiRoutes.Projects.UpdateService, UpdateService);
+        //group.MapPost(ProjectsApiRoutes.Projects.UpdateSettings, UpdateSettings);
+
+        if (debugMode)
+            Console.WriteLine($"{GetType().Name}.{nameof(UseServices)} Finished");
+
+        return true;
+    }
+
+    //// POST api/projects/updatesettings
+    //private static async Task<IResult> UpdateSettings([FromBody] UpdateSettingsRequest? request,
+    //    HttpRequest httpRequest, IMediator mediator, IMessagesDataManager messagesDataManager,
+    //    CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(UpdateSettings)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(UpdateSettingsCommandHandler)} from {nameof(UpdateSettings)}");
+
+    //    if (request is null)
+    //        return Results.BadRequest(new[] { ApiErrors.RequestIsEmpty });
+
+    //    var command = request.AdaptTo(userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(UpdateSettings)} finished", cancellationToken);
+    //    return result.Match(_ => Results.Ok(), Results.BadRequest);
+    //}
+
+    //// POST api/projects/update
+    //private static async Task<IResult> Update([FromBody] ProjectUpdateRequest? request, HttpRequest httpRequest,
+    //    IMediator mediator, IMessagesDataManager messagesDataManager, CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(Update)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(ProjectUpdateCommandHandler)} from {nameof(Update)}");
+
+    //    if (request is null)
+    //        return Results.BadRequest(new[] { ApiErrors.RequestIsEmpty });
+    //    var command = request.AdaptTo(userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(Update)} finished", cancellationToken);
+    //    return result.Match(Results.Ok, Results.BadRequest);
+    //}
+
+    //// POST api/projects/updateservice
+    //private static async Task<IResult> UpdateService([FromBody] UpdateServiceRequest? request, HttpRequest httpRequest,
+    //    IMediator mediator, IMessagesDataManager messagesDataManager, CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(UpdateService)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(UpdateServiceCommandHandler)} from {nameof(UpdateService)}");
+
+    //    if (request is null)
+    //        return Results.BadRequest(new[] { ApiErrors.RequestIsEmpty });
+    //    var command = request.AdaptTo(userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(UpdateService)} finished", cancellationToken);
+    //    return result.Match(Results.Ok, Results.BadRequest);
+    //}
+
+    //// POST api/projects/stop/{projectName}/{environmentName}
+    //private static async Task<IResult> StopService([FromRoute] string projectName, [FromRoute] string environmentName,
+    //    HttpRequest httpRequest, IMediator mediator, IMessagesDataManager messagesDataManager,
+    //    CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(StopService)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(StopServiceCommandHandler)} from {nameof(StopService)}");
+
+    //    var command = StopServiceCommandRequest.Create(projectName, environmentName, userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(StopService)} started", cancellationToken);
+    //    return result.Match(_ => Results.Ok(), Results.BadRequest);
+    //}
+
+    //// POST api/projects/start/{projectName}/{environmentName}
+    //private static async Task<IResult> StartService([FromRoute] string projectName, [FromRoute] string environmentName,
+    //    HttpRequest httpRequest, IMediator mediator, IMessagesDataManager messagesDataManager,
+    //    CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(StartService)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(StartServiceCommandHandler)} from {nameof(StartService)}");
+
+    //    var command = StartServiceCommandRequest.Create(projectName, environmentName, userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(StartService)} started", cancellationToken);
+    //    return result.Match(_ => Results.Ok(), Results.BadRequest);
+    //}
+
+    //// POST api/projects/removeprojectservice/{projectName}/{environmentName}
+    //private static async Task<IResult> RemoveProjectService([FromRoute] string projectName,
+    //    [FromRoute] string environmentName, [FromRoute] bool isService, HttpRequest httpRequest, IMediator mediator,
+    //    IMessagesDataManager messagesDataManager, CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(RemoveProjectService)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(RemoveProjectServiceCommandHandler)} from {nameof(RemoveProjectService)}");
+
+    //    var command = RemoveProjectServiceCommandRequest.Create(projectName, environmentName, isService, userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(RemoveProjectService)} finished", cancellationToken);
+    //    return result.Match(_ => Results.Ok(), Results.BadRequest);
+    //}
+
+    //// GET api/projects/getappsettingsversion/{serverSidePort}/{apiVersionId}
+    //private static async Task<IResult> GetAppSettingsVersion([FromRoute] int serverSidePort,
+    //    [FromRoute] string apiVersionId, HttpRequest httpRequest, IMediator mediator,
+    //    IMessagesDataManager messagesDataManager, CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(GetAppSettingsVersion)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(GetAppSettingsVersionQueryHandler)} from {nameof(GetAppSettingsVersion)}");
+
+    //    if (string.IsNullOrWhiteSpace(apiVersionId) || serverSidePort == 0)
+    //        return Results.BadRequest(new[] { ProjectsErrors.SameParametersAreEmpty });
+
+    //    var command = GetAppSettingsVersionQueryRequest.Create(serverSidePort, apiVersionId, userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(GetAppSettingsVersion)} finished", cancellationToken);
+    //    return result.Match(ret => Results.Text(ret, "text/plain", Encoding.UTF8), Results.BadRequest);
+    //}
+
+    //// POST api/projects/getversion/{serverSidePort}/{apiVersionId}
+    //private static async Task<IResult> GetVersion([FromRoute] int serverSidePort, [FromRoute] string apiVersionId,
+    //    HttpRequest httpRequest, IMediator mediator, IMessagesDataManager messagesDataManager,
+    //    CancellationToken cancellationToken = default)
+    //{
+    //    var userName = httpRequest.HttpContext.User.Identity?.Name;
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(GetVersion)} started", cancellationToken);
+    //    Debug.WriteLine($"Call {nameof(GetVersionQueryHandler)} from {nameof(GetVersion)}");
+
+    //    if (string.IsNullOrWhiteSpace(apiVersionId) || serverSidePort == 0)
+    //        return Results.BadRequest(new[] { ProjectsErrors.SameParametersAreEmpty });
+
+    //    var command = GetVersionQueryRequest.Create(serverSidePort, apiVersionId, userName);
+    //    var result = await mediator.Send(command, cancellationToken);
+
+    //    await messagesDataManager.SendMessage(userName, $"{nameof(GetVersion)} finished", cancellationToken);
+    //    return result.Match(ret => Results.Text(ret, "text/plain", Encoding.UTF8), Results.BadRequest);
+    //}
+}
