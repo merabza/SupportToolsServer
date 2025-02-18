747ef3a14a9b1bd486034627b3dc1cd58855f4dd
merabza <merabza@gmail.com>
2025-02-18 23:59:05 +0400

    202502182358

--- SupportToolsServerApi/Endpoints/V1/GitEndpoints.cs
+++ SupportToolsServerApi/Endpoints/V1/GitEndpoints.cs
@@ -2,40 +2,39 @@
 using SupportToolsServerContracts.V1.Routes;
 using WebInstallers;
 
-namespace SupportToolsServerApi.Endpoints.V1
+namespace SupportToolsServerApi.Endpoints.V1;
+
+internal class GitEndpoints : IInstaller
 {
-    internal class GitEndpoints : IInstaller
+    public int InstallPriority => 50;
+    public int ServiceUsePriority => 50;
+
+    public bool InstallServices(WebApplicationBuilder builder, bool debugMode, string[] args,
+        Dictionary<string, string> parameters)
     {
-        public int InstallPriority => 50;
-        public int ServiceUsePriority => 50;
+        return true;
+    }
 
-        public bool InstallServices(WebApplicationBuilder builder, bool debugMode, string[] args,
-            Dictionary<string, string> parameters)
-        {
-            return true;
-        }
+    public bool UseServices(WebApplication app, bool debugMode)
+    {
+        if (debugMode)
+            Console.WriteLine($"{GetType().Name}.{nameof(UseServices)} Started");
 
-        public bool UseServices(WebApplication app, bool debugMode)
-        {
-            if (debugMode)
-                Console.WriteLine($"{GetType().Name}.{nameof(UseServices)} Started");
+        var group = app.MapGroup(SupportToolsServerApiRoutes.ApiBase + SupportToolsServerApiRoutes.Projects.ProjectBase)
+            .RequireAuthorization();
 
-            var group = app.MapGroup(SupportToolsServerApiRoutes.ApiBase + SupportToolsServerApiRoutes.Projects.ProjectBase)
-                .RequireAuthorization();
+        //group.MapGet(ProjectsApiRoutes.Projects.GetAppSettingsVersion, GetAppSettingsVersion);
+        //group.MapGet(ProjectsApiRoutes.Projects.GetVersion, GetVersion);
+        //group.MapDelete(ProjectsApiRoutes.Projects.RemoveProjectService, RemoveProjectService);
+        //group.MapPost(ProjectsApiRoutes.Projects.StartService, StartService);
+        //group.MapPost(ProjectsApiRoutes.Projects.StopService, StopService);
+        //group.MapPost(ProjectsApiRoutes.Projects.Update, Update);
+        //group.MapPost(ProjectsApiRoutes.Projects.UpdateService, UpdateService);
+        //group.MapPost(ProjectsApiRoutes.Projects.UpdateSettings, UpdateSettings);
 
-            //group.MapGet(ProjectsApiRoutes.Projects.GetAppSettingsVersion, GetAppSettingsVersion);
-            //group.MapGet(ProjectsApiRoutes.Projects.GetVersion, GetVersion);
-            //group.MapDelete(ProjectsApiRoutes.Projects.RemoveProjectService, RemoveProjectService);
-            //group.MapPost(ProjectsApiRoutes.Projects.StartService, StartService);
-            //group.MapPost(ProjectsApiRoutes.Projects.StopService, StopService);
-            //group.MapPost(ProjectsApiRoutes.Projects.Update, Update);
-            //group.MapPost(ProjectsApiRoutes.Projects.UpdateService, UpdateService);
-            //group.MapPost(ProjectsApiRoutes.Projects.UpdateSettings, UpdateSettings);
+        if (debugMode)
+            Console.WriteLine($"{GetType().Name}.{nameof(UseServices)} Finished");
 
-            if (debugMode)
-                Console.WriteLine($"{GetType().Name}.{nameof(UseServices)} Finished");
-
-            return true;
-        }
+        return true;
     }
 }
