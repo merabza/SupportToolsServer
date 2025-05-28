c2e3fe204987de433e7f575311f7a8c22c95c5c8
merabza <merabza@gmail.com>
2025-05-28 19:32:46 +0400

    202505281931

---
+++ SupportToolsServerApi/Handlers/GetOneGitRepoQueryHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatRMessagingAbstractions;
+using OneOf;
+using SupportToolsServerApi.QueryRequests;
+using SupportToolsServerApiContracts.Models;
+using SupportToolsServerDom;
+using SystemToolsShared.Errors;
+
+namespace SupportToolsServerApi.Handlers;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed class GetOneGitRepoQueryHandler : IQueryHandler<GetOneGitRepoQueryRequest, GitDataDto>
+{
+    private readonly IGitsRepository _gitsRepo;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public GetOneGitRepoQueryHandler(IGitsRepository gitsRepo)
+    {
+        _gitsRepo = gitsRepo;
+    }
+
+    public async Task<OneOf<GitDataDto, IEnumerable<Err>>> Handle(GetOneGitRepoQueryRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        // Assumes request.RecordKey exists; adjust as needed
+        return await _gitsRepo.GetGitRepoByKey(request.RecordKey, cancellationToken);
+    }
+}
