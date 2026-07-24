54f0d65dc7046bacf3ef906d06f9c8145f719a1c
merabza <merabza@gmail.com>
2026-07-24 17:31:06 +0400

    202607241730

---
+++ SupportToolsServer.Persistence/DependencyInjection/SupportToolsServerDbDependencyInjection.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace SupportToolsServer.Persistence.DependencyInjection;
+
+// ReSharper disable once UnusedType.Global
+public static class SupportToolsServerDbDependencyInjection
+{
+    public static IServiceCollection AddSupportToolsServerDb(this IServiceCollection services, ILogger? debugLogger,
+        IConfiguration configuration)
+    {
+        debugLogger?.Information("{MethodName} Started", nameof(AddSupportToolsServerDb));
+
+        const string connectionStringConfigKey = "Data:SupportToolsServerDatabase:ConnectionString";
+        var connectionString = configuration[connectionStringConfigKey];
+
+        if (string.IsNullOrWhiteSpace(connectionString) && debugLogger is not null)
+        {
+            debugLogger.Error("Parameter {ConnectionStringConfigKey} is empty", connectionStringConfigKey);
+            return services;
+        }
+
+        services.AddDbContext<SupportToolsServerDbContext>(options => options.UseSqlServer(connectionString));
+        debugLogger?.Information("{MethodName} Finished", nameof(AddSupportToolsServerDb));
+
+        return services;
+    }
+}
